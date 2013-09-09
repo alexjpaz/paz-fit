a817ea3a32b87a1bf1b51bf393d1233623bd5fad
Alex Paz <alexander.j.paz@ngc.com>
2013-09-09 10:13:56 -0400

    :(

--- web-static.py
+++ web-static.py
@@ -5,7 +5,6 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'src/main/webapp/')
 
 app = Flask(__name__, static_folder=STATIC_ROOT, static_url_path='')
-app.debug=True
 
 @app.route('/')
 def hello():
