bce134998aff64588ed1185ea7753dfef46d7b35
Alex Paz <alexander.j.paz@ngc.com>
2013-12-03 12:23:24 -0500

    index

--- models.py
+++ models.py
@@ -2,17 +2,18 @@ from google.appengine.ext import ndb
 from google.appengine.ext import db
 
 class Note(db.Expando):
+	owner = db.UserProperty()
 	pass
 
 class Max(db.Model):
+	owner = db.UserProperty()
 	press = db.IntegerProperty()
 	deadlift = db.IntegerProperty()
 	bench = db.IntegerProperty()
 	squat = db.IntegerProperty()
-	user = db.UserProperty()
 	
 class PersonalRecord(db.Model):
+	owner = db.UserProperty()
 	max = db.IntegerProperty()
 	reps = db.IntegerProperty()
 	lift = db.StringProperty()
-	user = db.UserProperty()
