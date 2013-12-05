6f4722150e9d1c4ae90bd4d2e1839db994d3c548
Alex Paz <alexander.j.paz@ngc.com>
2013-12-05 08:35:59 -0500

    re

--- assets/components/c-calendar.js
+++ assets/components/c-calendar.js
@@ -1,10 +1,23 @@
 App.config(function(ComponentFactoryProvider) {
 	var ComponentFactory = ComponentFactoryProvider.$get();
 	ComponentFactory.build('c-calendar', function($scope) {
+
+		(function populate_calendar() {
+			$scope.days = [];
+
+			for(var i=0;i<(7*5);i++) {
+				$scope.days.push(i);
+			}
+		})();
+
 		$scope.events = {
 			'2013-12-1' : 'hi',
 			'2013-12-5' : 'hi',
 			'2013-12-7' : 'hi',
+		};
+
+		$scope.selectDay = function(day) {
+			console.log(day);
 		};
 
 		var derp = '2013-12-5';
--- assets/helper/enum.js
+++ assets/helper/enum.js
@@ -3,6 +3,7 @@ angular.module('app').config(function($provide) {
 		function ApplicationEnum() {
 			this.LIFT = ['press','deadlift','bench','squat'];
 			this.WEEK = ['3x5','3x3','531','DL'];
+
 		}
 		var instance = new ApplicationEnum();
 		$rootScope.ApplicationEnum = instance;
