bb0c54c45948c0262da4352ac2b6fdc10194b18a
Alexander Paz <alexjpaz@gmail.com>
2013-12-04 20:21:01 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,21 +1,21 @@
 from google.appengine.ext import ndb
 from google.appengine.ext import db
 
-class Note(db.Expando):
+class Post(db.Expando):
 	modified_date = db.DateProperty(auto_now=True)
 	creation_date = db.DateProperty(auto_now_add=True)
+	title = db.StringProperty()
 	content = db.StringProperty()
-	pass
-
+	
 class Max(db.Model):
 	press = db.IntegerProperty()
 	deadlift = db.IntegerProperty()
 	bench = db.IntegerProperty()
 	squat = db.IntegerProperty()
-	date = db.DateProperty(auto_now=True)
+	date = db.DateProperty(auto_now_add=True)
 	
 class PersonalRecord(db.Model):
 	weight = db.IntegerProperty()
 	reps = db.IntegerProperty()
 	lift = db.StringProperty()
-	date = db.DateProperty(auto_now=True)
+	date = db.DateProperty(auto_now_add=True)
