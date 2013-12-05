e00ab59423039571fd072ae02da7a737de2abc39
Alex Paz <alexander.j.paz@ngc.com>
2013-12-05 13:37:53 -0500

    using repo

--- assets/components/maxes.js
+++ assets/components/maxes.js
@@ -1,15 +1,14 @@
 App.config(function(ComponentFactoryProvider) {
 	var ComponentFactory = ComponentFactoryProvider.$get();
 	ComponentFactory.build('maxes', function($scope, RestRepository) {
-		RestRepository.find('Max').then(function(response) {
-			$scope.maxes = response.list.Max[0];
-		});
+		function getPersonalRecords(action) {
+			RestRepository[action]('Max').then(function(entity) {
+				$scope.maxes = response.list.Max[0];
+			});
+		}
 
 		$scope.refresh = function() {
-			RestRepository.refresh('Max').then(function(entity) {
-				$scope.dto = entity;
-			});
+			getPersonalRecords('refresh');
 		};
-
 	});
 });
--- assets/components/pr-list.js
+++ assets/components/pr-list.js
@@ -1,14 +1,14 @@
 App.config(function(ComponentFactoryProvider) {
 	var ComponentFactory = ComponentFactoryProvider.$get();
 	ComponentFactory.build('pr-list', function($scope, RestRepository) {
-		RestRepository.find('PersonalRecord').then(function(entity) {
-			$scope.dto = entity;
-		});
-
-		$scope.refresh = function() {
-			RestRepository.refresh('PersonalRecord').then(function(entity) {
+		function getPersonalRecords(action) {
+			RestRepository[action]('PersonalRecord').then(function(entity) {
 				$scope.dto = entity;
 			});
+		}
+
+		$scope.refresh = function() {
+			getPersonalRecords('refresh');
 		};
 	});
 });
