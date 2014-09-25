371e479e9b86e5b68dcfe03beb533fa15520734d
Alex Paz <alexjpaz@gmail.com>
2014-09-25 16:05:45 -0400

    ci fix

--- drone.sh
+++ drone.sh
@@ -4,5 +4,5 @@ npm install
 ./node_modules/.bin/grunt
 
 popd
-sed -i "s/{{GIT_COMMIT}}/${GIT_COMMIT:=master}/" src/app.yaml
-sed -i "s/{{BUILD_ID}}/${BUILD_ID:=none}/" src/app.yaml
+sed -i "s/\$GIT_COMMIT/${GIT_COMMIT:=master}/" src/app.yaml
+sed -i "s/\$BUILD_ID/${BUILD_ID:=none}/" src/app.yaml
