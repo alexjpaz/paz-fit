52b983f1756abd7fb7a0dfd10893c07b3a5b586c
Alex Paz <alexjpaz@gmail.com>
2014-10-21 14:42:48 -0400

    adding this back. Need to decouple ydn

--- src/node/gruntfile.js
+++ src/node/gruntfile.js
@@ -32,6 +32,7 @@ module.exports = function (grunt) {
 						'bower_components/bootstrap/dist/js/bootstrap.min.js',
 						'bower_components/d3/d3.min.js',
 						'bower_components/momentjs/min/moment.min.js',
+						'../assets/lib/ydn.db-iswu-core-e-qry-dev.js'
 						],
 						dest: 'dist/lib.js',
 				},
