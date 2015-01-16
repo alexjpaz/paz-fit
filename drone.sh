bbadd34ef242a178b885637d58f212bf78399bd5
Alex Paz <alexjpaz@gmail.com>
2015-01-16 11:00:13 -0500

    ci deploy branches

--- drone.sh
+++ drone.sh
@@ -6,3 +6,4 @@ npm install
 popd
 sed -i "s/\$GIT_COMMIT/${GIT_COMMIT:=master}/" src/app.yaml
 sed -i "s/\$BUILD_ID/${BUILD_ID:=none}/" src/app.yaml
+sed -i "s/\$GIT_BRANCH/${GIT_BRANCH:=test}/" src/app.yaml
