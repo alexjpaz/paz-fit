e7a4f0dbff445bd627faeb2918b91073bf7f608f
Alex Paz <alexjpaz@gmail.com>
2015-01-21 14:08:20 -0500

    maxes sheets with accessory

--- src/assets/components/sheets-accessory.js
+++ src/assets/components/sheets-accessory.js
@@ -2,8 +2,30 @@ angular.module('app').config(function(ComponentFactoryProvider) {
 	var ComponentFactory = ComponentFactoryProvider.$get();
 	ComponentFactory.build('sheets-accessory', {
 		scope: {
-			'mdl': '=sheetsAccessory',
 			'name': '@'
+		},
+		require: 'ngModel',
+		link: function(scope, element, attrs, ngModel) {
+			scope.mdl = [];
+			scope.add = function() {
+				scope.mdl.push({ 
+					value: 100 
+				});
+			};
+
+			scope.$watch('mdl', function(mdl) {
+				var arr = [];
+				angular.forEach(mdl, function(mm) {
+					if(!mm) return;
+					arr.push(mm.value);
+				});
+				ngModel.$setViewValue(arr);
+			}, true);
+
+			//ngModel.$parsers.push(function(value) {
+				//console.debug(value);
+				//scope.mdl = value;
+			//});
 		}
 	});
 });
