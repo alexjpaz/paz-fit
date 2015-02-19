0693a351777c38edd05df270e4af8dfee259dbd0
Alex Paz <alexjpaz@gmail.com>
2015-02-19 11:42:50 -0500

    allow panel header to have custom buttons

--- src/assets/components/panel.js
+++ src/assets/components/panel.js
@@ -3,10 +3,27 @@ angular.module('app').config(function(ComponentFactoryProvider){
 	ComponentFactory.build('panel', {
 		replace: true,
 		transclude: true,
-		controller: function($scope) {
-			$scope.c = {};
-			$scope.c.hideBody = false;
-		},
-		scope: {'panelTitle':'@'}
+		scope: {'panelTitle':'@'},
+		compile: function(cElement) {
+			return {
+				post: function(scope, iElement, iAttrs, controller, transcludeFn) {
+					scope.c = {};
+					scope.c.hideBody = false;
+
+					transcludeFn(function(tElement) {
+						var tElements = [
+							'panel-header',
+							'panel-content'
+						];
+
+
+						angular.forEach(tElements, function(el) {
+							console.debug(cElement.html())
+							cElement.find(el).replaceWith(tElement.filter(el));
+						});
+					});
+				}
+			};
+		}
 	});
 });
